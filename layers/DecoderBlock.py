cf53fcbd7813f2912696c13fab13e62ef1c0ca09
Hevensh <86734333+Hevensh@users.noreply.github.com>
2023-04-29 09:27:14 +0800

    Update DecoderBlock.py

--- layers/DecoderBlock.py
+++ layers/DecoderBlock.py
@@ -1,5 +1,16 @@
 from tensorflow.keras import Model, layers
 
+class DecoderLSTMcell(Model):
+    def __init__(self,numFilters):
+        super().__init__()
+        self.fc1 = layers.Dense(numFilters, activation='sigmoid', use_bias=False)
+        self.fc2 = layers.Dense(numFilters, activation='sigmoid', use_bias=False)
+        
+    def call(self, inputs):
+        h = self.fc1(inputs)
+        y = self.fc2(h)
+        return h, y
+
 class DecoderBlock(Model):
     def __init__(self,numFilters):
         super().__init__()

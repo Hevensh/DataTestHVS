20397f7a28000963227f6f01f99c457573072925
Hevensh <86734333+Hevensh@users.noreply.github.com>
2023-04-29 09:18:35 +0800

    Create ResidualBlock.py

---
+++ layers/ResidualBlock.py
@@ -0,0 +1,12 @@
+from tensorflow.keras import Model, layers
+
+class ResidualBlock(Model):
+    def __init__(self,numFilters=NUM_FILTERS,sizeFilters=3):
+        super().__init__()
+        self.conv1 = layers.Conv1D(numFilters, sizeFilters, activation='relu', padding='causal')
+        self.conv2 = layers.Conv1D(numFilters, sizeFilters, activation='relu', padding='causal')
+        
+    def call(self, inputs):
+        x1 = self.conv1(inputs)
+        x2 = self.conv2(x1)
+        return x2 + inputs
